9b5b4e6004f9c0a0893713190f0526a3094e608a
Sébastien KALINA <sebastien.kalina@weave.eu>
2019-07-18 18:31:14 +0200

    Fix disk_mount.sh

--- terraform/provider/gcp/gce-database/scripts/disk_mount.sh
+++ terraform/provider/gcp/gce-database/scripts/disk_mount.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e
 
-LOCALVOLDEVICE=$(readlink -f /dev/disk/by-id/google-*local*)
-CONTAINERSDEVICE=$(readlink -f /dev/disk/by-id/google-*containers*)
-LOCALDIR="/var/lib/origin/openshift.local.volumes"
-CONTAINERSDIR="/var/lib/docker"
+DATABASEDEVICE=$(readlink -f /dev/disk/by-id/google-*database*)
+BACKUPDEVICE=$(readlink -f /dev/disk/by-id/google-*backup*)
+DATABASEDIR="/var/lib/origin/openshift.local.volumes"
+BACKUPDIR="/var/lib/docker"
 
-for device in ${LOCALVOLDEVICE} ${CONTAINERSDEVICE}
+for device in ${DATABASEDEVICE} ${BACKUPDEVICE}
 do
   if [ "$(sudo blkid -s TYPE -o value ${device})" != "xfs" ]; then
     sudo mkfs.xfs -f ${device}
   fi
 done
 
-for dir in ${LOCALDIR} ${CONTAINERSDIR}
+for dir in ${DATABASEDIR} ${BACKUPDIR}
 do
   sudo mkdir -p ${dir}
   sudo restorecon -R ${dir}
 done
 
-sudo echo UUID=$(sudo blkid -s UUID -o value ${LOCALVOLDEVICE}) ${LOCALDIR} xfs defaults,discard,gquota 0 2 | sudo tee -a /etc/fstab
-sudo echo UUID=$(sudo blkid -s UUID -o value ${CONTAINERSDEVICE}) ${CONTAINERSDIR} xfs defaults,discard 0 2 | sudo tee -a /etc/fstab
+sudo echo UUID=$(sudo blkid -s UUID -o value ${DATABASEDEVICE}) ${DATABASEDIR} xfs defaults,discard,gquota 0 2 | sudo tee -a /etc/fstab
+sudo echo UUID=$(sudo blkid -s UUID -o value ${BACKUPDEVICE}) ${BACKUPDIR} xfs defaults,discard 0 2 | sudo tee -a /etc/fstab
 
 sudo mount -a
